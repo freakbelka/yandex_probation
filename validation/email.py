fee171faf05dcaf80f2b5d3824ce99c49293a226
Alina Berdnikova <freakbelka@yandex.ru>
2014-02-14 16:19:47 +0400

    Module

---
+++ validation/email.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# Модуль проверки валидности e-mail
+import re
+
+# Функция проверки попадания символов '!,:' в парные двойные кавычки
+def checkquote(name):
+    if re.search(r'["!:,]', name):
+        quotes = re.findall(r'["!:,]', name) 
+
+        symbols = '' 
+        flag = True
+        lst = [] 
+
+        # Преобразовываем список в строку
+        for i in quotes:
+            symbols += i
+
+        # Создаем список индексов всех элементов строки, не являющихся символом "
+        for i in range(len(quotes)):
+            if quotes[i] != '"':
+                lst.append(i)
+        
+        # Проверяем наличие нечетного количества кавычек перед и как минимум одной после каждого элемента строки
+        for i in range(len(lst)):
+            if len(re.findall(r'"', symbols[:lst[i]])) % 2 != 0 and symbols[lst[i]+1:].find(r'"') != -1:
+                flag = flag and True
+            else:
+                flag = flag and False
+        return not flag 
+    else:
+        return False
+
+# Основная функция проверки e-mail на соответствие критериям
+def emailcheck(name, domain):
+    match_name = re.match(r'[a-z\d"\_\-\.\:\!\,]{1,128}$', name)
+    match_domain = re.match(r'^[^\-\.@][a-z\d\_\-\.]{3,256}$', domain)
+
+    # Проверка соответствия условию для имени
+    if not match_name:
+        return False
+    elif  '..' in name or len(re.findall(r'"', name)) % 2 != 0 or checkquote(name):
+        return False
+
+    # Проверка соответствия условию для доменной части
+    if not match_domain:
+        return False
+    elif '.-' in domain or '-.' in domain or domain.endswith('-') or domain.endswith('.'):
+        return False
+   
+    return True
+
+# Функция отделяет username от hostname и вызывает функцию для проверки валидности введеного e-mail
+def splitting(email):
+	name = email[0:email.find('@')]
+	domain = email[email.find('@')+1:]
+	print (email)
+
+	if emailcheck(name,domain):
+		print ("SUCCESS \n")
+	else:
+		print ("FAILED \n")
+
